fc5f74c130b191280901b1a4b14688b7ae897559
Asier3237 <asiermartinezdelgado@gmail.com>
2025-12-09 02:37:31 +0100

    Intrefaz admin terminada, con distintos menus (a falta de funcionalidad)

---
+++ app/src/main/java/com/example/hogwartsasiermartinez/viewModel/FragmentoPerfilViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.hogwartsasiermartinez
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hogwartsasiermartinez.Api.UserAPI
+import com.example.hogwartsasiermartinez.Api.UserNetwork
+import com.example.hogwartsasiermartinez.model.Usuario
+import kotlinx.coroutines.launch
+
+class PerfilViewModel : ViewModel() {
+
+    private val _usuario = MutableLiveData<Usuario>()
+    val usuario: LiveData<Usuario> = _usuario
+
+    fun cargarPerfil(usuarioId: Int) {
+        viewModelScope.launch {
+            try {
+                val response = UserNetwork.retrofit.getUsuario(usuarioId)
+                if (response.isSuccessful) {
+                    _usuario.value = response.body()
+                }
+
+            } catch (e: Exception) {
+                // Manejo de errores
+            }
+        }
+    }
+}
